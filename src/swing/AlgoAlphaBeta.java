e10318f754320215f435ae084295ba31a693f378
Alice <alice.foraison@hotmail.fr>
2021-12-06 18:01:12 +0100

    commit_Alice
    Commentaires et mise en forme AlgoAlphaBeta DONE

--- src/swing/AlgoAlphaBeta.java
+++ src/swing/AlgoAlphaBeta.java
@@ -1,3 +1,9 @@
+/*
+ * Partie algorithme de notre projet Alpha-Beta
+ * Algorithme basé sur Min-Max avec élagague des branches qui
+ * ne conduisent pas à l'objectif de départ (pour un gain de temps)
+ */
+
 package swing;
 
 import java.util.ArrayList;
@@ -6,15 +12,13 @@ import java.util.Collections;
 import java.util.List;
 
 
-
 public class AlgoAlphaBeta {
-	// l'algorithme alpha beta se base sur - infini / + infini ici on prendra les
-	// valeurs max de min et max value
-	// INT_MAX Maximum value for a variable of type int. 2147483647
-	public static final int MAX = Integer.MAX_VALUE;
-	// INT_MIN Minimum value for a variable of type int. -2147483648
-	public static final int MIN = Integer.MIN_VALUE;
-	//private static final int PROFONDEUR_EXPLORATION_ALPHA_BETA = 4;
+	
+	//valeurs minimale et maximale de Java pour représenter les infinis
+	public static final int MAX = Integer.MAX_VALUE; // 2147483647
+	public static final int MIN = Integer.MIN_VALUE; //-2147483648
+	
+	
 	private int niveau ;
 
 	public int getNiveau() {
@@ -29,40 +33,52 @@ public class AlgoAlphaBeta {
 		this.niveau = lvl;
 	}
 
+	
+//----------------------------------------------------------------------------------------
+	/*ouJouer : création d'une copie de la grille courante et insertion d'un jeton pour chaque colonne
+	 * 
+	 * @param JoueurAbstrait joueur : joueur actuel
+	 * @param Grille grille : grille de jeu actuelle
+	 * 
+	 * @return int : numéro de colonne au plus grand poids
+	 */
 	public int ouJouer(JoueurAbstrait joueur, Grille grille) {
 
-		// ArrayList<Integer> colonnesJouables = grille.colonesJouables();
-		double valeurDeJeu = MIN;
-		int alphaMin = MIN, betaMax = MAX, colAjouer = 0;
-		int poidsColonne[] = new int[Grille.LARGEUR];
-			List<Integer> PoissibiliteDeJeu = new ArrayList<Integer>();
-		int profondeurArbre = grille.getTourDeQuelJoueur().getNiveau();
-		int colGagnante = verifCoupGagnant( joueur,  grille);
-		if (colGagnante>=0) {
+		double 			valeurDeJeu 		= MIN;
+		int 			alphaMin 			= MIN;
+		int 			betaMax 			= MAX;
+		int 			poidsColonne[] 	  	= new int[Grille.LARGEUR];
+		List<Integer> 	PoissibiliteDeJeu 	= new ArrayList<Integer>();
+		int 			colGagnante 	  	= verifCoupGagnant( joueur,  grille);
+		
+		//si on a une colonne gagnante, on la retourne comme résultat direcement
+		if (colGagnante>=0) 
+		{
 			return colGagnante;
 		}
-		// on va faire le le calcul pouur chaque colonne de la grille
+		
+		//parcour de la grille en largeur = par colonne
 		for (int i = 0; i < Grille.LARGEUR; i++) {
-			// on regarde si la col est pleine si c'est plein pas besoin de copier la grille
-			// pour cette colonne donc next
 			
+			//si la colonne n'est pas pleine
 			if (!grille.colPleine(i) ) {
-			//	System.out.print("\n Colonne ----------------" + i );
-				// copie de la grille (grille courante a la boucle for)
+				
+				//copie de la grille courante
 				Grille copieDeLaGrille = grille.Copie();				
-				
-				// on recupére la profondeur de l'arbre qu'on va gen (pour l'humain l'aide de
-				// profondeur 4 et pour L'IA son niveau)
-				
+				//insertion d'un jeton dans la colonne 
 				copieDeLaGrille.insere(i, joueur.getSymbole());
-			//	System.out.print("\n base " +joueur.getSymbole());
+				
 				int valeurDeJeuCourante;
-					if (!copieDeLaGrille.Victoire(joueur.getSymbole(),grille.getTourDeQuelJoueur().getSymbole()))
-				{ valeurDeJeuCourante = min(joueur, copieDeLaGrille, alphaMin, betaMax, this.niveau);}
+				
+				//si victoire ? calcul du MIN : sinon calcul MAX
+				if (!copieDeLaGrille.Victoire(joueur.getSymbole(),grille.getTourDeQuelJoueur().getSymbole()))
+				{ 
+					valeurDeJeuCourante = min(joueur, copieDeLaGrille, alphaMin, betaMax, this.niveau);}
 				else {
 					valeurDeJeuCourante =AlgoAlphaBeta.MAX;
 				}
-			//	valeurDeJeuCourante = min(joueur, copieDeLaGrille, alphaMin, betaMax, PROFONDEUR_EXPLORATION_ALPHA_BETA);
+					
+				//si la valeur calculée = MIN alors la colonne peut être jouée
 				if (valeurDeJeuCourante == valeurDeJeu) {
 					PoissibiliteDeJeu.add(i);
 				} else if (valeurDeJeuCourante > valeurDeJeu) {
@@ -70,108 +86,155 @@ public class AlgoAlphaBeta {
 					PoissibiliteDeJeu.clear();
 					PoissibiliteDeJeu.add(i);
 				}
+				
+				//colonne suivante
 				poidsColonne[i] = valeurDeJeuCourante;
 								
 			} else {
-			//	System.out.print("\n outofbound  colonne  ----------------"  );
+				//si la colonne est pleine, directement MIN
 				poidsColonne[i] = alphaMin;						
 			}
-			
 		}
+		//retours de la liste des poids des colonnes
 		System.out.print("\n Colonne ----------------"+Arrays.toString(poidsColonne) );
+		
+		//
 		Collections.shuffle(PoissibiliteDeJeu);
 		grille.setPoidsColonnes(poidsColonne);
 		return PoissibiliteDeJeu.get(0);
 	}
+	
+	
+//----------------------------------------------------------------------------------------
+	/*verifCoupGagnant : numéro de colonne si victoire au prochain coup
+	 * 
+	 * @param JoueurAbstrait joueur : joueur actuel
+	 * @param Grille grille : grille de jeu actuelle
+	 * 
+	 * @return int 
+	 */
 	private int verifCoupGagnant(JoueurAbstrait joueur, Grille grille) {
+		
+		//parcour de la grille en largeur = par colonne
 		for (int i = 0; i < Grille.LARGEUR; i++) {
-			if (!grille.colPleine(i) ) {
-			//	System.out.print("\n Colonne ----------------" + i );
-				// copie de la grille (grille courante a la boucle for)
+			
+			//si la colonne n'est pas pleine
+			if (!grille.colPleine(i)) {
+				
 				Grille copieDeLaGrille = grille.Copie();
 				copieDeLaGrille.insere(i, joueur.getSymbole());
-							int valeurDeJeuCourante;
-				if (copieDeLaGrille.Victoire(joueur.getSymbole(),grille.getTourDeQuelJoueur().getSymbole()))
-				{return i;}
-		
-				}		
-			}
+				
+				if (copieDeLaGrille.Victoire(joueur.getSymbole(),grille.getTourDeQuelJoueur().getSymbole())) {
+					return i;
+				}
+			}		
+		}
 		return -1;
 	}
 
-// algo min de alpha beta, ici on applique la valeur min
+	
+//----------------------------------------------------------------------------------------
+	/*min : calcul du MIN dans notre algo AlphaBeta
+	 * 
+	 * @param JoueurAbstrait joueur : joueur actuel
+	 * @param Grille grille : grille de jeu actuelle
+	 * @param int alpha : MIN
+	 * @param int beta : MAX
+	 * @param int occurence : profondeur de recherche
+	 * 
+	 * @return int : valeur de MIN
+	 */
 	private int min(JoueurAbstrait joueur, Grille grille, int alpha, int beta, int occurence) {
-	//	System.out.print("\n min  "+  occurence+' '+ grille.getNombreDeTour()+"+ alpha/beta +"+alpha +' '+ beta);
-
+		
+		//répétition pour une profondeure donnée
 		if (occurence >= 0) {
-		//s	System.out.print("\n occurence  "+ occurence);
+			
 			int valeurDeJeu = MAX;
+			
+			//parcour de la grille en largeur = par colonne
 			for (int i = 0; i < grille.LARGEUR; i++) {
+				
 				if (!grille.colPleine(i)) {
+					//création d'une copie de la grille actuelle et insérsion d'un jeton pour chaque colonne
 					Grille copieDeLaGrille = grille.Copie();
-					//occurence--;
-					//System.out.print("\n min " +copieDeLaGrille.getTourDeQuelJoueur().getSymbole());
 					copieDeLaGrille.insere(i, copieDeLaGrille.getTourDeQuelJoueur().getSymbole());
-
+					
+					//appel récursif de la partie MAX dans MIN
 					valeurDeJeu = Math.min(valeurDeJeu, this.max(joueur, copieDeLaGrille, alpha, beta, occurence-1));
-					// doit on tronquer la branche ?
-					if ( alpha >=valeurDeJeu  ) {
-						// ici on tronque la branche on ira donc pas plus loin dans min
-						//break;
+					
+					//vérification si besoin d'élaguer la branche
+					if ( alpha >=valeurDeJeu) {
+						//si branche tronquée, arrêt des calculs
 						return valeurDeJeu;
 					}
+					//calcul du MIN
 					beta = Math.min(beta, valeurDeJeu);
 				}
 			}
+			
 			return valeurDeJeu;
+			
 		} else {
-
 			int result = grille.poids(joueur);
-			//System.out.print("\n min result  "+ result);
-
 			return result;
 		}
 	}
+	
 
-	// algo max de alpha beta, ici on applique la valeur max
+//----------------------------------------------------------------------------------------
+	/*max : calcul du MAX dans notre algo AlphaBeta
+	 * 
+	 * @param JoueurAbstrait joueur : joueur actuel
+	 * @param Grille grille : grille de jeu actuelle
+	 * @param int alpha : MIN
+	 * @param int beta : MAX
+	 * @param int occurence : profondeur de recherche
+	 * 
+	 * @return int : valeur de MAX
+	 */
 	private int max(JoueurAbstrait joueur, Grille grille, int alpha, int beta, int occurence) {
-	//	System.out.print("\n max  "+  occurence+' '+ grille.getNombreDeTour()+"+ alpha/beta +"+alpha +' '+ beta);
- 
-		if (occurence >= 0) {
-		//	System.out.print("\n max occurence  "+ occurence);
+		
+		if (occurence >= 0) { 
 			int valeurDeJeu = MIN;
+			
 			for (int i = 0; i < grille.LARGEUR; i++) {
+				
 				if (!grille.colPleine(i)) {
-					// init des variables
+					//création d'une copie de la grille actuelle et insérsion d'un jeton pour chaque colonne
 					Grille copieDeLaGrille = grille.Copie();
-				//	System.out.print("\n max " +copieDeLaGrille.getTourDeQuelJoueur().getSymbole());
-					// copie insersion dans les grilles
 					copieDeLaGrille.insere(i, copieDeLaGrille.getTourDeQuelJoueur().getSymbole());
+					
+					//appel récursif de la partie MIN dans MAX
 					valeurDeJeu = Math.max(valeurDeJeu, this.min(joueur, copieDeLaGrille, alpha, beta, occurence-1));
-					// doit on tronquer la branche ?o
+					
+					//vérification si besoin d'élaguer la branche
 					if (valeurDeJeu >= beta) {
-						// ici on tronque la branche on ira donc pas plus loin dans max
-						//break;
+						//si branche tronquée, arrêt des calculs
 						return valeurDeJeu;
 					}
+					//calcul du MAX
 					alpha = Math.max(alpha, valeurDeJeu);
 				}
 			}
+			
 			return valeurDeJeu;
+			
 		} else {
 			int result = grille.poids(joueur);
-			//grille.afficheGrille();
-			//System.out.print("\n max result  "+ result);
 			return result;
 		}
 	}
 
 
+//----------------------------------------------------------------------------------------
+	/*main : méthode pour les statistiques du projet, création d'une partie entre un Humain et un Ordinateur
+	 * 
+	 * @return void
+	 */
 	public static void main(String[] args) {
 		
-		long debut = System.currentTimeMillis();
-		boolean win = true;
-		Grille grille = new Grille();
+		Grille 	grille 	= new Grille();
+		
 		JoueurAbstrait joueurA = new Ordinateur('X', 8);
 		joueurA.setNom(" Humain");
 		JoueurAbstrait joueurB = new Ordinateur('O', 4);
@@ -182,74 +245,27 @@ public class AlgoAlphaBeta {
 		grille.afficheGrille();
 		JoueurAbstrait JoueurActuel = grille.getTourDeQuelJoueur();
 		JoueurAbstrait JoueurSuivant = grille.getTourJoueurSuivant();
-
-		
-		/*	for (int i = 0; i < LARGEUR; i++) {
-			for (int j = grille.dernierSymbole(i); j < LONGUEUR; j++) {
-				System.out.print("\ni , "+ i + " j "+ j + " valeur"+ grille.getcase(i, j) );
-			}
-		}*/		
 		
 		boolean gagner=false; 
-		while(!gagner ) {
+		while( !gagner ) {
 			int colonneAJouer;
 			
 			colonneAJouer = JoueurActuel.placerChar(grille);
 			grille.insere(colonneAJouer,JoueurActuel.getSymbole());
+			
 			if (grille.Victoire(JoueurActuel.getSymbole(), JoueurSuivant.getSymbole())) {
 				grille.afficheGrille();
-				gagner =true;
+				gagner=true;
 				System.out.print("Bravo c'est gagné "+JoueurActuel.getSymbole() );
-			}else {
-			grille.afficheGrille();
-			 JoueurActuel = grille.getTourDeQuelJoueur();
-			 JoueurSuivant = grille.getTourJoueurSuivant();
-			
+				
+			} else {
+				
+				grille.afficheGrille();
+				JoueurActuel = grille.getTourDeQuelJoueur();
+				JoueurSuivant = grille.getTourJoueurSuivant();
+			}	
 		}	
-		
-	}	
-		
-		
-		/*
-		
-		long debut = System.currentTimeMillis();
-		boolean win = true;
-		Grille grille = new Grille();
-		JoueurAbstrait joueurA = new Humain('x', 4);
-		joueurA.setNom("Joueur 1 ");
-		JoueurAbstrait joueurB = new Ordinateur('O', 4);
-	
-			
-		joueurB.setNom("Joueur 2 ");
-		grille.setJoueur1(joueurA);
-		grille.setJoueur2(joueurB);
-		grille.afficheGrille();
-		System.out.print(" tour "+ grille.getNombreDeTour());
-	
-		int numeroColonneOuJouer =  joueurA.placerChar(grille) ;
-		System.out.print("\n numeroColonneOuJouer  "+numeroColonneOuJouer);
-
-		grille.insere(4, grille.getTourDeQuelJoueur().getSymbole());
-		grille.insere(4, grille.getTourDeQuelJoueur().getSymbole());
-		grille.insere(4, grille.getTourDeQuelJoueur().getSymbole());
-		grille.insere(4, grille.getTourDeQuelJoueur().getSymbole());
-		//grille.insere(1,joueurA.getSymbole());	
-		
-		grille.afficheGrille();
-		System.out.print("\n numeroColonneOuJouer  "+  numeroColonneOuJouer +" tour "+ grille.getNombreDeTour());*/
-	/*	grille.insere(numeroColonneOuJouer,joueurA.getSymbole());	
-		grille.afficheGrille();
-		System.out.println("\n"+ (System.currentTimeMillis()-debut));*/ 
-	/*	while (win) {
-		
-			grille.afficheGrille();
-			JoueurAbstrait JoueurActuel = grille.getTourDeQuelJoueur();
-			JoueurAbstrait JoueurSuivant = grille.getTourJoueurSuivant();
-			
-					int numeroColonneOuJouer = JoueurActuel.placerChar(grille);
-			grille.insere(numeroColonneOuJouer,JoueurActuel.getSymbole());	
-			
-		}*/
 	}
 
+	
 }
